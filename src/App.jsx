949e0fb1b75ddcfd2114bf727cb22b1915ea5cfd
mbatia muniu <mbatiamnew@gmail.com>
2020-07-09 00:47:39 +0300

    components_init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import Greet from './components/Greet';
+// or
+// import Mycomponent from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+import Message from './components/Message';
+
+
+function App() {
+    return (
+                  <div className = "App">
+                  <Message></Message>
+              { /*<Greet name="Bruce" heroName="Batman"> <p> This is children props </p></Greet> 
+     <Greet name = "Clark" heroName = 'Superman'> <button>Action </button></Greet>
+        <Greet name = "Diana" heroName = 'Wonder woman'/>
+        <Welcome name = "Bruce" heroName='Batman'/>
+        <Welcome name = "Clark" heroName = 'Superman'/>
+    <Welcome name = "Diana" heroName = 'Wonder woman'/>*/}
+        </div>
+    );
+}
+
+export default App
